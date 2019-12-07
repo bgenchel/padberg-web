1bfbecdfe9a30ee62c59154c9b608232915f146a
bgenchel <benjiegenchel@gmail.com>
2019-12-07 18:42:31 -0500

    Updates

---
+++ precache-manifest.ab193da42699e2ff5b6c9120ef2da014.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66074c2a552ef80e97e481b4bf3ddb13",
+    "url": "/padberg-web/index.html"
+  },
+  {
+    "revision": "d96b247fc9a503ead3d6",
+    "url": "/padberg-web/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f8419a9f0162ca934285",
+    "url": "/padberg-web/static/js/2.48f1fd83.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/padberg-web/static/js/2.48f1fd83.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d96b247fc9a503ead3d6",
+    "url": "/padberg-web/static/js/main.25735cd9.chunk.js"
+  },
+  {
+    "revision": "9d82a897e7005fc86742",
+    "url": "/padberg-web/static/js/runtime-main.bff7d419.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/padberg-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
